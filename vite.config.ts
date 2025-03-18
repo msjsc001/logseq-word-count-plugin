a94708d011e72371dcc033754aa8187f3b401d80
msjsc001 <93178788+msjsc001@users.noreply.github.com>
2025-03-18 23:47:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
+   import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    lib: {
-      entry: 'index.ts',
-      formats: ['iife'],
-      name: 'WordCount'
-    }
-  }
-}) 
+   export default defineConfig({
+     build: {
+       target: 'esnext',
+       minify: 'esbuild',
+       lib: {
+         entry: 'index.ts',
+         formats: ['cjs'],
+         fileName: () => 'index.js',
+       },
+       rollupOptions: {
+         external: ['@logseq/libs'],
+       },
+     },
+   })
