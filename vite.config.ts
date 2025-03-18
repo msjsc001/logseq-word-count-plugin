20bffd78f739dde53a75e138bd49693bdd573a62
msjsc001 <93178788+msjsc001@users.noreply.github.com>
2025-03-18 23:52:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-   import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 
-   export default defineConfig({
-     build: {
-       target: 'esnext',
-       minify: 'esbuild',
-       lib: {
-         entry: 'index.ts',
-         formats: ['cjs'],
-         fileName: () => 'index.js',
-       },
-       rollupOptions: {
-         external: ['@logseq/libs'],
-       },
-     },
-   })
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    lib: {
+      entry: 'index.ts',
+      formats: ['cjs'],
+      fileName: () => 'index.js',
+    },
+    rollupOptions: {
+      external: ['@logseq/libs'],
+    },
+  },
+})
